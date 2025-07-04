36c22573a612dfbe5d506531062d728598e61012
julianbb <1546450025@qq.com>
2025-07-04 17:45:06 +0700

    change PWA14

--- components/sidebar-user-nav.tsx
+++ components/sidebar-user-nav.tsx
@@ -22,11 +22,13 @@ import { useRouter } from 'next/navigation';
 import { toast } from './toast';
 import { LoaderIcon } from './icons';
 import { guestRegex } from '@/lib/constants';
+import { useSidebar } from '@/components/ui/sidebar';
 
 export function SidebarUserNav({ user }: { user: User }) {
   const router = useRouter();
   const { data, status } = useSession();
   const { setTheme, resolvedTheme } = useTheme();
+  const { setOpenMobile } = useSidebar();
 
   const isGuest = guestRegex.test(data?.user?.email ?? '');
 
@@ -95,6 +97,7 @@ export function SidebarUserNav({ user }: { user: User }) {
                   }
 
                   if (isGuest) {
+                    setOpenMobile(false);
                     router.push('/login');
                   } else {
                     signOut({
