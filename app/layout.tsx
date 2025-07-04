40c6144254e1b43756f524f1d2420e3952476f3b
julianbb <1546450025@qq.com>
2025-07-04 17:38:40 +0700

    change PWA13

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,13 +69,13 @@ export default async function RootLayout({
           }}
         />
 
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         {/* <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Strategy" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no" /> */}
+        <meta name="theme-color" content="#ffffff" /> */}
 
 
 
