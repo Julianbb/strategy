cd0a7b1ea4283d3d5aaa8375642658ca344411f4
julianbb <1546450025@qq.com>
2025-07-06 00:19:41 +0700

    changed sidebar

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -1,27 +1,9 @@
-import { cookies } from 'next/headers';
-
-import { AppSidebar } from '@/components/app-sidebar';
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
-import { auth } from '../(auth)/auth';
-
-
-export const experimental_ppr = true;
+import AdminPanelLayout from "@/components/admin-panel/admin-panel-layout";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [session, cookieStore] = await Promise.all([auth(), cookies()]);
-  // const isCollapsed = cookieStore.get('sidebar:state')?.value !== 'true';
-  const isCollapsed = false
-
-  return (
-    <>
-      <SidebarProvider defaultOpen={!isCollapsed}>
-        <AppSidebar user={session?.user} />
-        <SidebarInset>{children}</SidebarInset>
-      </SidebarProvider>
-    </>
-  );
+  return <AdminPanelLayout>{children}</AdminPanelLayout>;
 }
--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -2,7 +2,6 @@
 import { auth } from '../(auth)/auth';
 import { redirect } from 'next/navigation';
 import { StrategyTypesSection } from '@/components/strategy-types-section';
-import { SidebarToggle } from '@/components/sidebar-toggle';
 
 export default async function Page() {
   const session = await auth();
@@ -13,13 +12,8 @@ export default async function Page() {
 
 
   return (
-    <>
-      <div className="items-center gap-2 p-2 flex lg:hidden">
-        <SidebarToggle />
-      </div>
       <div className="flex flex-col gap-6 p-6">
         <StrategyTypesSection />
       </div>
-    </>
   );
 }
