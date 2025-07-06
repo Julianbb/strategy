44ee4da6c6bc0cb140e9490450d92be155f87b3d
julianbb <1546450025@qq.com>
2025-07-06 20:45:41 +0700

    add  PWA manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
     >
       <head>
         <script dangerouslySetInnerHTML={{ __html: THEME_COLOR_SCRIPT }} />
-        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link rel="manifest" href="/manifest.json" />
         {/* <meta name="theme-color" content="#ffffff" /> */}
         {/* <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> */}
