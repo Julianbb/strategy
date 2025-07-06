646ea55947dbd994867f964315d576c883069446
julianbb <1546450025@qq.com>
2025-07-06 17:22:30 +0700

    PWA6

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,6 +101,6 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography'),require('tailwindcss-safe-area')],
 };
 export default config;
