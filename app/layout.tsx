38c3771a815e4aec56b865ae9f993671a9cfca3f
julianbb <1546450025@qq.com>
2025-07-06 18:00:33 +0700

    PWA12

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'/>
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
