6472969cd9dda4a34a46732187c3884cdbf7d57b
julianbb <1546450025@qq.com>
2025-07-06 18:32:23 +0700

    PWA14

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,8 +60,8 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: THEME_COLOR_SCRIPT }} />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#ffffff" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        {/* <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> */}
         <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'/>
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
